60347525c54fdf6413f069b774c51226208be6be
LeHieu <ledinhhieu.936@gmail.com>
2021-11-08 01:59:42 +0700

    save

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Champion from './Components/Champion';
+import Infor from './Components/Infor';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Search from './Components/Search';
+import Skeleton from 'react-loading-skeleton';
+function App() {
+  const [Heros, setHero] = useState([])
+  const [ConstHeros, setConstHeros] = useState([])
+  useEffect(() => {
+    const GetAPI = async () => {
+      const URL = "https://ddragon.leagueoflegends.com/cdn/11.21.1/data/vn_VN/champion.json";
+      const reponse = await fetch(URL);
+      const reponseJSON = await reponse.json();
+      const data = reponseJSON.data;
+      const element = [];
+      for (const key in data) {
+        if (Object.hasOwnProperty.call(data, key)) {
+          element.push(key);
+        }
+      }
+      setHero(element);
+      setConstHeros(element);
+    }
+    GetAPI();
+  }, [])
+  const SearchHero = name => {
+    let newname = name.toLowerCase();
+    let newHeros = []
+    for (let hero of ConstHeros) {
+      let newhero = hero.toLowerCase();
+      if (newhero.indexOf(newname) > -1)
+        newHeros.push(hero)
+    }
+    console.log(newHeros)
+    setHero(newHeros)
+  }
+
+  return (
+    <div className="container">
+      <Search func={SearchHero} />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+
+            <div className="list-Champion">
+              {Heros.map(hero => (
+                <Champion hero={hero} key={hero} />
+              )) || <Skeleton count={5} />
+              }
+            </div>
+          </Route>
+          {Heros.map(hero => {
+            let url = `/${hero}`
+            return (
+              <Route exact path={url} key={hero}>
+                <Infor hero={hero}></Infor>
+              </Route>
+            )
+          })}
+        </Switch>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
